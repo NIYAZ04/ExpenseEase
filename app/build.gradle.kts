e06f9599c9b3b1d85885f6505d1038afb8367e97
NIYAZ <niyazhaque098@gmail.com>
2023-08-13 07:38:11 -0700

    DayFour_Completed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.expensetracker"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
@@ -51,7 +51,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
@@ -67,8 +67,11 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
-    // Dependencies added by Mir Niyazul Haque
+
+    // Dependencies added by Mir_Niyazul_Haque
     // Navigation Dependency
 
     implementation("androidx.navigation:navigation-compose:2.6.0")
+
+    implementation("com.marosseleng.android:compose-material3-datetime-pickers:0.6.0")
 }
--- app/src/main/java/com/example/expensetracker/pages/Add.kt
+++ app/src/main/java/com/example/expensetracker/pages/Add.kt
@@ -2,8 +2,11 @@ package com.example.expensetracker.pages
 // Mir Niyazul Haque
 // This app is going to Rock !!
 import android.app.DatePickerDialog
-import android.widget.DatePicker
+import android.os.Build
+
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -27,11 +30,13 @@ import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
@@ -40,34 +45,39 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.DialogProperties
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.expensetracker.Components.TableRow
 import com.example.expensetracker.Components.UnstyledTextField
+import com.example.expensetracker.models.Recurrence
 import com.example.expensetracker.ui.theme.BackgroundElevated
 import com.example.expensetracker.ui.theme.DividerColor
 import com.example.expensetracker.ui.theme.ExpenseTrackerTheme
 import com.example.expensetracker.ui.theme.Primary
 import com.example.expensetracker.ui.theme.Shapes
 import com.example.expensetracker.ui.theme.TopAppBarBackground
+import com.example.expensetracker.viewmodels.AddViewModel
+import com.marosseleng.compose.material3.datetimepickers.date.ui.dialog.DatePickerDialog
+import java.time.LocalDate
 import java.util.Calendar
+import kotlin.math.roundToInt
 
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
 @Composable
-fun Add(navController:NavController)
+fun Add(navController:NavController, vm: AddViewModel= viewModel())
 { // For Recurrence Tab
-    val recurrences = listOf(
-        "None",
-        "Daily",
-        "Weekly",
-        "Monthly",
-        "Yearly"
-    )
 
-    var selectedRecurrence by remember{
-        mutableStateOf(recurrences[0])
-    }
+    val state by vm.uiState.collectAsState()
+    val recurrences = listOf(
+        Recurrence.None,
+        Recurrence.Daily,
+        Recurrence.Weekly,
+        Recurrence.Monthly,
+        Recurrence.Yearly
+    )
 
     // For Category Tab
     val categories = listOf(
@@ -77,44 +87,6 @@ fun Add(navController:NavController)
         "Take Out",
         "Party"
     )
-    var selectedCategory by remember{
-        mutableStateOf(categories[0])
-    }
-    // Making Calender (Date ) Selector
-
-    val mContext= LocalContext.current
-    val mYear: Int
-    val mMonth: Int
-    val mDay: Int
-
-    val mCalendar = Calendar.getInstance()
-
-    mYear= mCalendar.get(Calendar.YEAR)
-    mMonth= mCalendar.get(Calendar.MONTH)
-    mDay= mCalendar.get(Calendar.DAY_OF_MONTH)
-
-    var mDate by remember{
-        mutableStateOf("${mCalendar.get(Calendar.DAY_OF_MONTH)}-${mCalendar.get(Calendar.MONTH)+1}-${mCalendar.get(Calendar.YEAR)}")
-    }
-    val mDatePicker = DatePickerDialog(
-        mContext,
-        { _:DatePicker, selectedYear: Int, selectedMonth:Int, selectedDay: Int->
-
-            mDate = "${selectedDay}-${selectedMonth +1}-${selectedYear}"
-        },
-        mYear,
-        mMonth,
-        mDay
-
-
-    )
-    mDatePicker.datePicker.maxDate = mCalendar.timeInMillis
-
-
-
-
-
-
 
     Scaffold (
         topBar = {
@@ -132,12 +104,15 @@ fun Add(navController:NavController)
                     .fillMaxWidth()
                     .background(BackgroundElevated)
                 ){
-                    TableRow("Amount"){
+                    TableRow("Amount"){  
 
 
-                        UnstyledTextField(value = "HELLO",
-                            onValueChange ={},
+                        UnstyledTextField(value =state.amount,
+                            onValueChange =vm::setAmount,
                             modifier=Modifier.fillMaxWidth() ,
+                            placeholder={ Text(text = "0")},
+                            arrangement=Arrangement.End,
+                            maxLines=1,
                             textStyle = TextStyle(
                                 textAlign = TextAlign.Right
                             ),
@@ -158,7 +133,7 @@ fun Add(navController:NavController)
 
                         )
                         {
-                            Text(text = selectedRecurrence)
+                            Text(state.recurrence?.name ?: Recurrence.None.name)
                             DropdownMenu(expanded =recurrenceMenuOpened ,
                                 onDismissRequest = {recurrenceMenuOpened=false }
                             )
@@ -166,9 +141,10 @@ fun Add(navController:NavController)
                                 recurrences.forEach{recurrence->
 
                                     DropdownMenuItem(
-                                        text = { Text(recurrence)},
+                                        text = { Text(recurrence.name)},
+
                                         onClick = {
-                                          selectedRecurrence=recurrence
+                                          vm.setRecurrence(recurrence)
                                             recurrenceMenuOpened= false
 
 
@@ -187,12 +163,30 @@ fun Add(navController:NavController)
                         modifier = Modifier
                             .padding(start = 1.dp), thickness = 1.dp, color = DividerColor
                     )
+                    var datePickerShowing by remember{
+                        mutableStateOf(false)
+                    }
                     TableRow("Date"){
 
-                        TextButton(onClick = {mDatePicker.show()}) {
-                            Text(mDate)
+                        TextButton(onClick = {datePickerShowing=true}) {
+                            Text(state.date.toString())
 
-                        }
+                       }
+                      if(datePickerShowing){
+
+                          DatePickerDialog(
+                              onDismissRequest = { datePickerShowing = false},
+                              onDateChange = {
+                                             it->
+                                  vm.setDate(it)
+                                  datePickerShowing = false
+                              },
+                              initialDate =state.date,
+                              title = { Text(text = "Select a Date", style = MaterialTheme.typography.titleLarge)}
+                          )
+                      }
+
+
                     }
 
                     Divider(
@@ -201,8 +195,11 @@ fun Add(navController:NavController)
                     )
                     TableRow("Note"){
 
-                        UnstyledTextField(value = "HELLO",
-                            onValueChange ={},
+                        UnstyledTextField(
+                            value = state.note,
+                            placeholder={ Text(text = "Add some Notes")},
+                            arrangement=Arrangement.End,
+                            onValueChange =vm::setNote,
 
                             modifier=Modifier.fillMaxWidth() ,
                             textStyle = TextStyle(
@@ -227,7 +224,7 @@ fun Add(navController:NavController)
 
                         )
                         {
-                            Text(text = selectedCategory)
+                            Text(state.category ?:"Select a Category ")
                             DropdownMenu(expanded =categoriesMenuOpened ,
                                 onDismissRequest = {categoriesMenuOpened=false }
                             )
@@ -249,7 +246,7 @@ fun Add(navController:NavController)
                                                },
 
                                         onClick = {
-                                            selectedCategory=category
+                                          vm.setCategory(category)
                                             categoriesMenuOpened= false
 
 
